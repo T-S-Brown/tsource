bd09bc830dd36ac87bbab87179e9741eeba3aa26
Thomas Brown <tom.southwell.brown@gmail.com>
2018-01-29 19:21:14 +0000

    Added data manipulation fundamentals code

---
+++ dplyr2pandas.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 09:18:29 2018
+
+@author: Thomas Brown
+"""
+
+#-----------------------------------------------------#
+# Dplyr to Pandas
+# Migrating fundamental data manipulation skills
+# from R to Python
+#
+# Thomas Brown, 2018
+#-----------------------------------------------------#
+
+# Load a module
+
+# R: library(dplyr)
+import pandas as pd
+
+# load the example dataset
+data = pd.read_csv("C:/Users/E610971/OneDrive - UCB/Desktop/survey.csv")
+
+
+#------------------------------#
+# Select
+#------------------------------#
+
+# Select Variables
+new_data = data[["Age", "Gender", "Country"]]
+
+# Drop Variables
+new_data = data.drop(["state"], axis = 1)
+
+# Starts/Ends with X
+new_data = data.loc[:, data.columns.str.startswith("self")]
+new_data = data.loc[:, data.columns.str.endswith("e")]
+
+# Contains X
+new_data = data.loc[:, data.columns.str.contains("health", regex = False)]
+
+# Matches a regular expression
+new_data = data.loc[:, data.columns.str.contains("^[A,G,s]")]
+
+
+#------------------------------#
+# Filter 
+#------------------------------#
+
+# Logical Filters
+new_data = data[data['Age'] > 40]
+new_data = data[data['state'] == 'CA']
+new_data = data[data['state'] != 'CA']
+
+# Multiple logical filters
+new_data = data[(data['Age'] > 10) & (data['Age'] < 60)]
+
+# Missing filters
+new_data = data[data.state.notnull()]
+new_data = data[data.state.isnull()]
+
+
+
+
+#------------------------------#
+# Arrange 
+#------------------------------#
+
+new_data = data.sort_values('state')
+new_data = data.sort_values('state', ascending = False)
+
+new_data = data.sort_values(['Age', 'state'], ascending = [False, True])
+
+
+
+#------------------------------#
+# Mutate
+#------------------------------#
+
+# Required to calculate various variables numerically
+import numpy as np
+
+new_data = data.copy()
+new_data['indicator'] = 1
+
+new_data['age2'] = np.array(data['Age'])*2
+
+
+
+
+#------------------------------#
+# Summarise
+#------------------------------#
+
+
