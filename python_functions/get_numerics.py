86aedd93ab09ab5162b1ff5b6370f167a8c7591b
Thomas Brown <tom.southwell.brown@gmail.com>
2018-03-22 09:39:51 +0000

    Update get_numerics.py

--- python_functions/get_numerics.py
+++ python_functions/get_numerics.py
@@ -1,6 +1,9 @@
 # Extract the Numeric Vectors
 
 # Input Dataset is a pandas DataFrame
+# Requires...
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
 def extract_numerics(data):
     out = pd.DataFrame()
