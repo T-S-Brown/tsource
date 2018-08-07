e050334f628752bb94f7e394b4ca21b30d17efcd
Thomas Brown <tom.southwell.brown@gmail.com>
2018-08-07 16:06:58 +0100

    create remove zero var function

---
+++ python_functions/remove_zero_var.py
@@ -0,0 +1,7 @@
+def remove_zero_var(data):
+    out = pd.DataFrame()
+    
+    for col in data.columns:
+        if np.var(data[col].dropna()) != 0:
+            out[col] = data[col]
+    return(out)
